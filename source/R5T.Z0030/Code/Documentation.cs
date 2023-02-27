a85a037a5b9852f7637068dc206572a85c853a63
davidcoats <david@getsafetycone.com>
2023-02-27 12:41:33 -0800

    Initial commit.

---
+++ source/R5T.Z0030/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.Z0030
+{
+	/// <summary>
+	/// Identity name values for types, methods, etc. (strongly-typed).
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
